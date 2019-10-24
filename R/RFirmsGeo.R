4be8aae12f1fd873738e3ba1565b25c361afe5ed
eblondel <emmanuel.blondel1@gmail.com>
2019-10-24 13:50:12 +0200

    pkg version/date

--- R/RFirmsGeo.R
+++ R/RFirmsGeo.R
@@ -10,8 +10,8 @@
 #' \tabular{ll}{
 #'    Package: \tab RFirmsGeo\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 0.2-1\cr
-#'    Date: \tab 2018-09-14\cr
+#'    Version: \tab 0.2-2\cr
+#'    Date: \tab 2019-10-24\cr
 #'    License: \tab GPL(>=2.0)\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
