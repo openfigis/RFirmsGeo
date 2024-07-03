a10642c40528034fc78e9abfce12c89f68e7e56d
eblondel <emmanuel.blondel1@gmail.com>
2024-07-03 11:45:07 +0200

    refactor readSpatialObject(s)

--- R/readSpatialObject.R
+++ R/readSpatialObject.R
@@ -8,9 +8,6 @@
 #' @param wfs instance of WFS client, object of class \code{WFSClient} from \pkg{ows4R}
 #' @param layer an object of class "data.frame" (single record) giving information
 #'        about a GIS web-layer to query
-#' @param cleanGeom an object of class "logical" indicating if geometries have to
-#'        be validated with \pkg{cleangeo}. Default value is TRUE.
-#' @param cleanStrategy an object of class "character". Default value is "BUFFER"
 #' @param verbose an object of class "logical" either logs have to be printed out.
 #'        Default value is TRUE.
 #' @return an object of class "spatial" giving the spatial object
@@ -18,8 +15,9 @@
 #' @note function used internally to build FIRMS spatial objects
 #' 
 #' @author Emmanuel Blondel, \email{emmanuel.blondel1@@gmail.com}
+#' @export
 #'
-readSpatialObject <- function(wfs, layer, cleanGeom = TRUE, cleanStrategy = "BUFFER", verbose = TRUE){
+readSpatialObject <- function(wfs, layer, verbose = TRUE){
   
   #get featuretype
   caps <- wfs$getCapabilities()
@@ -65,10 +63,7 @@ readSpatialObject <- function(wfs, layer, cleanGeom = TRUE, cleanStrategy = "BUF
   }
   if(!is.null(out)) if(nrow(out)==0L) out <- NULL
   if(!is.null(out)){
-    out <- as(out, "Spatial")
-    if(cleanGeom){
-      out <- clgeo_Clean(out, strategy = cleanStrategy)
-    }
+    out <- sf::st_make_valid(out)
   }else{
     logger.warn("Unknown or Empty filtered GIS web-resource")  
   }
@@ -86,8 +81,6 @@ readSpatialObject <- function(wfs, layer, cleanGeom = TRUE, cleanStrategy = "BUF
 #' @param layers an object of class "data.frame" (single record) giving information
 #'        about a GIS web-layer to query
 #' @param cleanGeom an object of class "logical" indicating if geometries have to
-#'        be validated with \pkg{cleangeo}. Default value is TRUE.
-#' @param cleanStrategy an object of class "character". Default value is "BUFFER"
 #' @param verbose an object of class "logical" either logs have to be printed out.
 #'        Default value is TRUE.
 #' @return an object of class "list" listing the spatial objects
@@ -95,13 +88,14 @@ readSpatialObject <- function(wfs, layer, cleanGeom = TRUE, cleanStrategy = "BUF
 #' @note function used internally to build FIRMS spatial objects
 #' 
 #' @author Emmanuel Blondel, \email{emmanuel.blondel1@@gmail.com}
+#' @export
 #'
-readSpatialObjects <- function(wfs, layers, cleanGeom = TRUE, cleanStrategy = TRUE, verbose = TRUE){
+readSpatialObjects <- function(wfs, layers, verbose = TRUE){
   logger.info("Read spatial objects...")
-  sp.layers <- lapply(unique(layers$typeName), function(x){
+  sf.layers <- lapply(unique(layers$typeName), function(x){
     geoitem <- layers[layers$typeName == x,]
-    sp <- readSpatialObject(wfs, geoitem, cleanGeom, cleanStrategy, verbose)
+    sp <- readSpatialObject(wfs, geoitem, verbose)
     return(sp)
   })
-  sp.layers <- sp.layers[!sapply(sp.layers, is.null)]
+  sf.layers <- sf.layers[!sapply(sf.layers, is.null)]
 }
