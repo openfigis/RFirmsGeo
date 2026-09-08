b39144c98c684f2cd08dd4cf882c5ad5b048552d
eblondel <emmanuel.blondel1@gmail.com>
2026-09-08 16:43:04 +0200

    troubleshoot grsf_areas querying

--- R/readSpatialObject.R
+++ R/readSpatialObject.R
@@ -35,6 +35,9 @@ readSpatialObject <- function(wfs, layer, verbose = TRUE){
     value <- gisItem$propertyValue
     if(!is.na(key) | !is.na(value)){
       gisFilter = sprintf("strToLowerCase(%s)='%s'", key, tolower(value))
+      if(layer$typeName == "grsf:grsf_areas"){
+        gisFilter = URLencode(sprintf("%s = '%s'", key, value))
+      }
       if(is.null(cqlFilter)){
         cqlFilter = gisFilter
       }else{
